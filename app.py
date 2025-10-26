b83d7bec40f77a8d3992949b8e6ee76318dcc1c0
Azwaad Labiba Mohiuddin <azwaad07@gmail.com>
2025-10-26 00:38:25 -0500

    Add .html extensions to frozen files

--- app.py
+++ app.py
@@ -7,30 +7,24 @@ def index():
     return render_template('index.html', title='About')
 
 @app.route('/education')
+@app.route('/education.html')
 def education():
     return render_template('education.html', title='Education')
 
 @app.route('/experience')
+@app.route('/experience.html')
 def experience():
     return render_template('experience.html', title='Experience')
 
 @app.route('/publications')
+@app.route('/publications.html')
 def publications():
     return render_template('publications.html', title='Publications')
 
 @app.route('/achievements')
+@app.route('/achievements.html')
 def achievements():
     return render_template('achievements.html', title='Achievements')
-
-# @app.route('/blog')
-# def blog():
-#     return render_template('blog.html', title='Blog')
-
-# @app.route('/blog/<slug>')
-# def blog_post(slug):
-#     # In a real app, you'd fetch the post from a database
-#     # For now, just render the template
-#     return render_template('blog_post.html', slug=slug)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
